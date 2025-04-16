c9a41a772be2578fbe43d69b6cf17615abf4d621
Hashidgaba <hashidgaba@gmail.com>
2025-04-16 20:27:15 +0500

    done

--- firstProgram/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        App obj = new App();
-        obj.sayHellow();
-        System.out.println("the sum of the numbers is : " + obj.sum(56, 10));
-    }
-}
