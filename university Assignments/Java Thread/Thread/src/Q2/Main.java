8283796b1efc5896c30b5ae71b7529cfb689cc49
Hashidgaba <hashidgaba@gmail.com>
2025-06-21 22:36:39 +0500

    done thread

---
+++ university Assignments/Java Thread/Thread/src/Q2/Main.java
@@ -0,0 +1,29 @@
+
+package Q2;
+
+public class Main {
+
+	public static void main(String[] args) {
+    Numberprinter N = new Numberprinter();
+    Characterprinter C = new Characterprinter();
+    N.run();
+    C.alphabets();
+
+	}
+}
+class Numberprinter extends Thread{
+	public void run () {
+		for(int i = 1;i<=5;i++) {
+			System.out.println(i);
+		}
+	}
+}
+class Characterprinter extends Thread{
+	public void alphabets() {
+		for (char c = 'A'; c <= 'E'; c++) {
+        System.out.print(c + " ");
+    }
+		
+	}
+	
+}
