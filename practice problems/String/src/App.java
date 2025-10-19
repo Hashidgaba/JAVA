07de43259cea9b546f6c9edc51e5983dd9d5c522
Hashidgaba <hashidgaba@gmail.com>
2025-10-19 22:30:54 +0500

    reverse string

--- practice problems/String/src/App.java
+++ practice problems/String/src/App.java
@@ -1,17 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
       String a = "Hello, World!";
-      String b = "This is Hashid gaba";
+      // String b = "This is Hashid gaba";
       // System.out.println(a); 
       // System.out.println(b);
       // System.out.println(a.length());
       // System.out.println(b.charAt(5));
       // System.out.println(b.indexOf("gaba"));
       // System.out.println(b.indexOf('a'));
-      System.out.println(b.substring(8).equalsIgnoreCase("Hashid gaba"));
-      System.out.println(a.length());
-      for(int i = a.length() - 1; i >= 0;i--){
-       System.out.print(a.charAt(i));
-      }
+      // System.out.println(b.substring(8).equalsIgnoreCase("Hashid gaba"));
+      // System.out.println(a.length());
+      // char[] arr = {'H', 'e', 'l', 'l', 'o'};
+      // String str = new String(arr);
+      // System.out.println(str);
+      //revers stirng using StringBuilder
+      // String name  = "Hashid gaba";
+      // System.out.println(new StringBuilder(name).reverse().toString());
+      // char[] arr = a.toCharArray();//it become char array
+      
     }
 }
